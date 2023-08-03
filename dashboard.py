f61900041e845edea123061ae709953f72f63486
mars12161 <108153152+mars12161@users.noreply.github.com>
2023-08-03 17:24:11 +0200

    added the pandas ai func

--- dashboard.py
+++ dashboard.py
@@ -6,6 +6,9 @@ Bayer Challenge Team D Life
 
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
 
 
 def read_data(filepath: str) -> pd.DataFrame:
@@ -16,6 +19,23 @@ df = read_data("data.csv")
 df = df.dropna(axis=1, how='all')
 
 
+def ask_pandas():
+    llm = OpenAI(api_token='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')
+    pandasai = PandasAI(llm)
+    with st.form("Question"):
+        question = st.text_input("Question", value="", type="default")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            with st.spinner("Thinking..."):
+                answer = pandasai.run(df, prompt=question)
+
+                fig = plt.gcf()
+                if fig.get_axes():
+                    st.pyplot(fig)
+                st.write(answer)
+
+
+
 def create_tabs():
     tab1, tab2, tab3 = st.tabs(['Overview', 'AI', 'Predictions'])
 
@@ -24,6 +44,8 @@ def create_tabs():
 
     with tab2:
         st.header("Ask the AI")
+        st.write("Here you can ask the AI a question about the data")
+        ask_pandas()
 
     with tab3:
         st.header("Predictions")
