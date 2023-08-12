567e43fad1e50886e6b02110ffe180038b0e629a
Karen Bolon <99751409+karenbolon@users.noreply.github.com>
2023-08-12 16:29:24 +0200

    updated for simpler data set for predictions

--- test.py
+++ test.py
@@ -1,123 +1,101 @@
 import streamlit as st
-#import pickle5 as pickle
 import pandas as pd
 import plotly.graph_objects as go
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 #from pandasai import PandasAI
-#import openai
-#from pandasai.llm.openai import OpenAI
+import openai
+from pandasai.llm.openai import OpenAI
 
-X_train_lr = pd.read_csv('./data/X_train_lr.csv')
-X_test_lr = pd.read_csv('./data/X_test_lr.csv')
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+from streamlit_option_menu import option_menu
+from sklearn.linear_model import LogisticRegression
+
+st.set_page_config(
+		page_title="Breast Cancer Predictor",
+		page_icon=":female-doctor:",
+		layout="wide",
+		initial_sidebar_state="expanded"
+	)
+
+X_train_rfc = pd.read_csv('./data/X_train_rfc_feature_elim.csv', index_col=None)
+X_test_rfc = pd.read_csv('./data/X_test_rfc_feature_elim.csv', index_col=None)
+y_train = pd.read_csv('./data/y_train.csv', index_col=None)['diagnosis'] 
+y_test = pd.read_csv('./data/y_test.csv',index_col=None)['diagnosis'] 
 
 def add_sidebar():
 	st.sidebar.header("Cell Nuclei Measurements")
 	slider_labels = [
 		("Radius (mean)", "radius_mean"),
-		("Texture (mean)", "texture_mean"),
 		("Perimeter (mean)", "perimeter_mean"),
-		("Area (mean)", "area_mean"),
-		("Smoothness (mean)", "smoothness_mean"),
-		("Compactness (mean)", "compactness_mean"),
-		("Concavity (mean)", "concavity_mean"),
-		("Concave points (mean)", "concave points_mean"),
-		("Radius (se)", "radius_se"),
-		("Texture (se)", "texture_se"),
-		("Perimeter (se)", "perimeter_se"),
+		("Concave points (mean)", "concave_points_mean"),
 		("Area (se)", "area_se"),
-		("Compactness (se)", "compactness_se"),
-		("Symmetry (se)", "symmetry_se"),
-		("Fractal dimension (se)", "fractal_dimension_se"),
 		("Radius (worst)", "radius_worst"),
-		("Texture (worst)", "texture_worst"),
-		("Perimeter (worst)", "perimeter_worst"),
 		("Area (worst)", "area_worst"),
-		("Smoothness (worst)", "smoothness_worst"),
-		("Concavity (worst)", "concavity_worst"),
-		("Concave points (worst)", "concave points_worst"),
-		("Symmetry (worst)", "symmetry_worst"),
-		("Fractal dimension (worst)", "fractal_dimension_worst"),
+		("Concave points (worst)", "concave_points_worst")
 	]
-
 	input_dict = {}
-
 	for label, key in slider_labels:
 		input_dict[key] = st.sidebar.slider(label, min_value=float(0),\
-			max_value=float(X_test_lr[key].max()), value=float(X_test_lr[key].mean())
+			max_value=float(X_test_rfc[key].max()), value=float(X_test_rfc[key].mean())
 	)
+	return input_dict
 
 def get_radar_chart(input_data):
-  
-  input_data = X_test_lr
-  
-  categories = ['Radius', 'Texture', 'Perimeter', 'Area', 
-				'Smoothness', 'Compactness', 
-				'Concavity', 'Concave Points',
-				'Symmetry', 'Fractal Dimension']
-
-  fig = go.Figure()
-
-  fig.add_trace(go.Scatterpolar(
-		r=X_test_lr,
+	categories = ['Radius', 'Perimeter', 'Area', 'Concave Points']
+	fig = go.Figure()
+	fig.add_trace(go.Scatterpolar(
+		r=[X_train_rfc[['radius_mean', 'perimeter_mean', 'concave_points_mean']]],
 		theta=categories,
 		fill='toself',
 		name='Mean Value'
-  ))
-  """fig.add_trace(go.Scatterpolar(
-		r=[
-		  input_data['radius_se'], input_data['texture_se'], input_data['perimeter_se'], input_data['area_se'],
-		  input_data['smoothness_se'], input_data['compactness_se'], input_data['concavity_se'],
-		  input_data['concave points_se'], input_data['symmetry_se'],input_data['fractal_dimension_se']
-		],
+	))
+	fig.add_trace(go.Scatterpolar(
+		r=[X_train_rfc['area_se']],
 		theta=categories,
 		fill='toself',
 		name='Standard Error'
-  ))
-  fig.add_trace(go.Scatterpolar(
-		r=[
-		  input_data['radius_worst'], input_data['texture_worst'], input_data['perimeter_worst'],
-		  input_data['area_worst'], input_data['smoothness_worst'], input_data['compactness_worst'],
-		  input_data['concavity_worst'], input_data['concave points_worst'], input_data['symmetry_worst'],
-		  input_data['fractal_dimension_worst']
-		],
+	))
+	fig.add_trace(go.Scatterpolar(
+		r=[X_train_rfc[['radius_worst', 'area_worst', 'concave_points_worst']]],
 		theta=categories,
 		fill='toself',
 		name='Worst Value'
-  ))"""
-
-  fig.update_layout(
-	polar=dict(
-	  radialaxis=dict(
-		visible=True,
-		range=[0, 1]
-	  )),
+	))
+	fig.update_layout(polar=dict(radialaxis=dict(visible=True, range=[0, 1])),
 	showlegend=True
-  )
-  
-  return fig
-
+	)
+	return fig
 
 def add_predictions(input_data):
-	lr = LogisticRegression(solver='liblinear', random_state = 12) 
-	lr.fit(X_train_lr, y_train)
+	lr = LogisticRegression(solver='liblinear', random_state=12) 
+	lr.fit(X_train_rfc, y_train)
 	scaler = StandardScaler()  
+
 	input_array = np.array(list(input_data.values())).reshape(1, -1)
-	input_array_scaled = scaler.transform(input_array)
-	prediction = model.predict(input_array_scaled)
+	input_array_scaled = scaler.fit_transform(input_array)
+	
+	prediction = lr.predict(input_array_scaled)
+	probabilities = lr.predict_proba(input_array_scaled)[0]	
+
 	st.subheader("Cell cluster prediction")
 	st.write("The cell cluster is:")
-
+	
 	if prediction[0] == 0:
 		st.write("<span class='diagnosis benign'>Benign</span>", unsafe_allow_html=True)
 	else:
 		st.write("<span class='diagnosis malicious'>Malicious</span>", unsafe_allow_html=True)
 	
-	st.write("Probability of being benign: ", lr.predict_proba(input_array_scaled)[0][0])
-	st.write("Probability of being malicious: ", lr.predict_proba(input_array_scaled)[0][1])
-
+	st.write("Probability of being benign: ", probabilities[0])
+	st.write("Probability of being malicious: ", probabilities[1])
+	
 	st.write("This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.")
-	#return (lr.predict_proba(input_array_scaled)[0][0], lr.predict_proba(input_array_scaled)[0][1])
+	return probabilities
 
 def assistant(B, M):
 	openai.api_key = "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"
@@ -137,24 +115,14 @@ def assistant(B, M):
 	)
 
 	guidelines = response.choices[0].text.strip()
-
+	return guidelines
 
 def main():
-  st.set_page_config(
-	#page_title="Breast Cancer Predictor",
-	page_icon=":female-doctor:",
-	layout="wide",
-	initial_sidebar_state="expanded"
-	)
-
-	input_data = add_sidebar()
-
+	input_data = add_sidebar()	
 	with st.container():
 		st.title("Breast Cancer Predictor")
 		st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
-
 		col1, col2 = st.columns([4,1])
-
 		with col1:
 			radar_chart = get_radar_chart(input_data)
 			st.plotly_chart(radar_chart)
