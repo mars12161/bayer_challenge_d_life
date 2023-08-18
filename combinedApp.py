a5a307eaf7096ba9907e7753fd18b4a3c7499af1
Mohammad Mohassel <m.mohassel1997@gmail.com>
2023-08-18 19:55:57 +0200

    adding and fixing prediction tab

--- combinedApp.py
+++ combinedApp.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import plotly.express as px
 import plotly.graph_objects as go
-import pickle
+import pickle5 as pickle
 import streamlit.components.v1 as components
 from streamlit_option_menu import option_menu
 from streamlit_extras.switch_page_button import switch_page
@@ -197,16 +197,16 @@ def sources_tab():
 		 https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7950292/,\n\
 		 https://canceratlas.cancer.org/,  \nhttps://ourworldindata.org/cancer  \n")
 
-def predictions_tab():
-	st.subheader('Predictions')
-	X_LR_fe = pd.read_csv('./data/X_LR_feature_reduction.csv')
+################################################
+################prediction tab##################
+################################################
 
-	def add_info():
-		st.subheader("Cell Nuclei Measurements")
-
-		slider_labels = [
+def add_info():
+	st.subheader("Cell Nuclei Measurements")
+	data = get_clean_data()
+	slider_labels = [
 			("Concavity (mean)", "concavity_mean"),
-			("Concave points (mean)", "concave_points_mean"),
+			("Concave points (mean)", "concave points_mean"),
 			("Radius (se)", "radius_se"),
 			("Perimeter (se)", "perimeter_se"),
 			("Area (se)", "area_se"),
@@ -215,147 +215,137 @@ def predictions_tab():
 			("Perimeter (worst)", "perimeter_worst"),
 			("Area (worst)", "area_worst"),
 			("Concavity (worst)", "concavity_worst"),
-			("Concave points (worst)", "concave_points_worst"),
-			("Symmetry (worst)", "symmetry_worst"),
-#			("Radius (mean)", "radius_mean"),
-#			("Texture (mean)", "texture_mean"),
-#			("Perimeter (mean)", "perimeter_mean"),
-#			("Area (mean)", "area_mean"),
-#			("Smoothness (mean)", "smoothness_mean"),
-#			("Compactness (mean)", "compactness_mean"),
-#			("Concavity (mean)", "concavity_mean"),
-#			("Concave points (mean)", "concave points_mean"),
-#			("Symmetry (mean)", "symmetry_mean"),
-#			("Fractal dimension (mean)", "fractal_dimension_mean"),
-#			("Radius (se)", "radius_se"),
-#			("Texture (se)", "texture_se"),
-#			("Perimeter (se)", "perimeter_se"),
-#			("Area (se)", "area_se"),
-#			("Smoothness (se)", "smoothness_se"),
-#			("Compactness (se)", "compactness_se"),
-#			("Concavity (se)", "concavity_se"),
-#			("Concave points (se)", "concave points_se"),
-#			("Symmetry (se)", "symmetry_se"),
-#			("Fractal dimension (se)", "fractal_dimension_se"),
-#			("Radius (worst)", "radius_worst"),
-#			("Texture (worst)", "texture_worst"),
-#			("Perimeter (worst)", "perimeter_worst"),
-#			("Area (worst)", "area_worst"),
-#			("Smoothness (worst)", "smoothness_worst"),
-#			("Compactness (worst)", "compactness_worst"),
-#			("Concavity (worst)", "concavity_worst"),
-#			("Concave points (worst)", "concave points_worst"),
-#			("Symmetry (worst)", "symmetry_worst"),
-#			("Fractal dimension (worst)", "fractal_dimension_worst"),
-		]
+			("Concave points (worst)", "concave points_worst"),
+			("Symmetry (worst)", "symmetry_worst")
+			]
 
-		input_dict = {}
+	input_dict = {}
 
-		for label, key in slider_labels:
-#			input_dict[key] = st.slider(label, min_value = float(0), max_value = float(data[key].max()), 
-#				value = float(data[key].mean())
-			input_dict[key] = st.slider(label, min_value = float(0), max_value = float(X_LR_fe[key].max()), 
-				value = float(X_LR_fe[key].mean())
-			)
-		return input_dict
+	for label, key in slider_labels:
+		input_dict[key] = st.slider(label, min_value=float(0), max_value=float(data[key].max()), 
+			      		value=float(data[key].mean())
+						)
+	return input_dict
+
+def get_clean_data():
+  data = pd.read_csv("data/data.csv")
+  
+  data = data.drop(["Unnamed: 32", "id", "radius_mean", "texture_mean", 
+                    "perimeter_mean", "area_mean", "smoothness_mean", "compactness_mean",
+                    "fractal_dimension_mean", "texture_se", "smoothness_se",
+                    "compactness_se", "concavity_se", "concave points_se" , "symmetry_se", "fractal_dimension_se", "smoothness_worst",
+                    "compactness_worst", "fractal_dimension_worst", "symmetry_mean"
+                    ], axis=1)
+  
+  data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
+  
+  return data
+
+def get_scaled_values(input_dict):
+  data = get_clean_data()
+  X = data.drop(['diagnosis'], axis=1)
+  
+  scaled_dict = {}
+  
+  for key, value in input_dict.items():
+    max_val = X[key].max()
+    min_val = X[key].min()
+    scaled_value = (value - min_val) / (max_val - min_val)
+    scaled_dict[key] = scaled_value
+  
+  return scaled_dict
 
 
-	def get_scaled_values(input_dict):
-		scaled_dict = {}
-
-		for key, value in input_dict.items():
-
-			max_val = X_LR_fe[key].max()
-			min_val = X_LR_fe[key].min()
-			scaled_value = (value - min_val) / (max_val - min_val)
-			scaled_dict[key] = scaled_value
-		return scaled_dict
-
-
-	def get_radar_chart(input_data):
-		input_data = get_scaled_values(input_data)
-
-		categories = ['Radius', 'Texture', 'Perimeter', 'Area', 
+def get_radar_chart(input_data):
+	input_data = get_scaled_values(input_data)
+	categories = ['Radius', 'Texture', 'Perimeter', 'Area', 
+			#	'Smoothness', 'Compactness', 
 				'Concavity', 'Concave Points',
-				'Symmetry'
+				'Symmetry' #'Fractal Dimension'
 				]
 
-		fig = go.Figure()
+	fig = go.Figure()
 
-		fig.add_trace(go.Scatterpolar(
+	fig.add_trace(go.Scatterpolar(
 			r=[
-			input_data['concavity_mean'], input_data['concave_points_mean'],0
+			0, 0, 0,
+			0,input_data['concavity_mean'], input_data['concave points_mean'], 0
 			],
 			theta=categories,
 			fill='toself',
 			name='Mean Value'
 		))
-		fig.add_trace(go.Scatterpolar(
+	fig.add_trace(go.Scatterpolar(
 			r=[
-			input_data['radius_se'], input_data['perimeter_se'], input_data['area_se'], 0
+			input_data['radius_se'],0, input_data['perimeter_se'], input_data['area_se'], 0,0,0
 			],
 			theta=categories,
 			fill='toself',
 			name='Standard Error'
 		))
-		fig.add_trace(go.Scatterpolar(
+	fig.add_trace(go.Scatterpolar(
 			r=[
 			input_data['radius_worst'], input_data['texture_worst'], input_data['perimeter_worst'],
-			input_data['area_worst'], input_data['concavity_worst'], input_data['concave_points_worst'], 
+			input_data['area_worst'], input_data['concavity_worst'], input_data['concave points_worst'], 
 			input_data['symmetry_worst']
 			],
 			theta=categories,
 			fill='toself',
 			name='Worst Value'
 		))
-		fig.update_layout(
+	fig.update_layout(
 			polar = dict(radialaxis = dict(visible = True,range = [0, 1])),
 			showlegend = True
 			)
-		return fig
+	return fig
 
-	def add_predictions(input_data):
-		model = pickle.load(open("./model/lr2.pkl", "rb"))
-		scaler = StandardScaler()  
-		input_array = np.array(list(input_data.values())).reshape(1, -1)
-		input_array_scaled = scaler.fit_transform(input_array)
-		prediction = model.predict(input_array_scaled)
-		st.subheader("Cell cluster prediction")
-		st.write("The cell cluster is:")
+def add_predictions(input_data):
+	model = pickle.load(open("./model/model.pkl", "rb"))
+	scaler = pickle.load(open("./model/scaler.pkl", "rb"))
+	input_array = np.array(list(input_data.values())).reshape(1, -1)
+	input_array_scaled = scaler.transform(input_array)
+	prediction = model.predict(input_array)
 
-		if prediction[0] == 0:
-			st.write("<span class='diagnosis benign'>Benign</span>", unsafe_allow_html=True)
-		else:
-			st.write("<span class='diagnosis malignant'>Malignant</span>", unsafe_allow_html=True)
+	st.subheader("Cell cluster prediction")
+	st.write("The cell cluster is:")
 
-		st.write("Probability of being benign: ",model.predict_proba(input_array_scaled)[0][0])
-		st.write("Probability of being malignant: ", model.predict_proba(input_array_scaled)[0][1])
-		st.write("This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.")
-		return (model.predict_proba(input_array_scaled)[0][0], model.predict_proba(input_array_scaled)[0][1])
+	if prediction[0] == 0:
+		st.write("<span class='diagnosis benign'>Benign</span>", unsafe_allow_html=True)
+	else:
+		st.write("<span class='diagnosis malignant'>Malignant</span>", unsafe_allow_html=True)
+	st.write("Probability of being benign: ", model.predict_proba(input_array_scaled)[0][0])
+	st.write("Probability of being malicious: ", model.predict_proba(input_array_scaled)[0][1])
+	st.write("This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.")
+	return (model.predict_proba(input_array_scaled)[0][0], model.predict_proba(input_array_scaled)[0][1])
 
-	def assistant(B, M):
-		openai.api_key = "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"
-		prompt = (
+def assistant(B, M):
+	openai.api_key = "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"
+	prompt = (
 			"I build an app with Wisconsin breast cancer diagnosis and used machine learning to give you these results, "
 			"now I want you to be in the role of assistant within that app and generate general guidelines "
 			"on what should he/she do when I give you the percentage now generate guidelines for these predictions "
 			"as you are talking to the patient:\n"
 			f"Prediction Results:\nMalignant Probability: {M}\nBenign Probability: {B}"
 			)
-		response = openai.Completion.create(
+	response = openai.Completion.create(
 			model="text-davinci-003",
 			prompt=prompt,
 			temperature=0.6,
 			max_tokens = 400
 			)
-		guidelines = response.choices[0].text.strip()
-		return(guidelines)
+	guidelines = response.choices[0].text.strip()
+	return(guidelines)
+
+col1, col2 = st.columns([1, 3])
+
+def predictions_tab():
+	st.subheader('Predictions')
 	with st.container():
 		st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app uses a logistic regression machine learning model to predict whether a breast mass is benign or malignant based on the measurements provided from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
-
-	col1, col2 = st.columns([1, 3])
 	with col1:
 		input_data = add_info()
+		if st.button('Predict!'):
+			st.write("bullshit")
 	with col2:
 		radar_chart = get_radar_chart(input_data)
 		st.plotly_chart(radar_chart)
@@ -366,7 +356,6 @@ def predictions_tab():
 		st.write("Here you can ask the AI a question about the data")
 		if st.button('Generate guidelines!'):
 			st.write(assistant(B, M))
-
 
 def find_exported_files(path):
 	for root, dirs, files in os.walk(path):
