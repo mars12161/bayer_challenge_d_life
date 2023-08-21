4b99908c3520059e19d619bd96a4eff0ac94f4a2
Karen Bolon <99751409+karenbolon@users.noreply.github.com>
2023-08-21 21:08:30 +0200

    updated model parameters to match the lr model on Mo s and the shap model names

--- model.py
+++ model.py
@@ -16,11 +16,11 @@ def create_model(data):
   
   # split the data
   X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.2, random_state=12
   )
   
   # train the model
-  model = LogisticRegression()
+  model = LogisticRegression(solver='liblinear', random_state = 12, stratify = y)
   model.fit(X_train, y_train)
   
   # test model
